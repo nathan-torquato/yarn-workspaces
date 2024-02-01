1cd606ebd9b140d3f65f9dc620d922149c6c0b61
Nathan Torquato <n.torquato.silva@gmail.com>
2024-02-01 09:59:53 -0300

    feat: add GraphQL server

---
+++ apps/core-portal/src/recipe.resolver.ts
@@ -0,0 +1,54 @@
+import {
+  Arg,
+  FieldResolver,
+  Int,
+  Mutation,
+  Query,
+  Resolver,
+  type ResolverInterface,
+  Root,
+} from "type-graphql";
+import { createRecipeSamples } from "./recipe.data";
+import { RecipeInput } from "./recipe.input";
+import { Recipe } from "./recipe.type";
+
+import { Car as SharedCar } from "@shared/models";
+@Resolver((_of) => Recipe)
+export class RecipeResolver implements ResolverInterface<Recipe> {
+  private readonly items: Recipe[] = createRecipeSamples();
+
+  @Query((_returns) => Recipe, { nullable: true })
+  async recipe(@Arg("title") title: string): Promise<Recipe | undefined> {
+    return this.items.find((recipe) => recipe.title === title);
+  }
+
+  @Query((_returns) => [Recipe], {
+    description: "Get all the recipes from around the world ",
+  })
+  async recipes(): Promise<Recipe[]> {
+    const sharedCar = new SharedCar(1, "car", 2021);
+    console.log("sharedCar", sharedCar);
+    return this.items;
+  }
+
+  @Mutation((_returns) => Recipe)
+  async addRecipe(@Arg("recipe") recipeInput: RecipeInput): Promise<Recipe> {
+    const recipe = Object.assign(new Recipe(), {
+      description: recipeInput.description,
+      title: recipeInput.title,
+      ratings: [],
+      creationDate: new Date(),
+    });
+    this.items.push(recipe);
+
+    return recipe;
+  }
+
+  @FieldResolver()
+  ratingsCount(
+    @Root() recipe: Recipe,
+    @Arg("minRate", (_type) => Int, { defaultValue: 0 }) minRate: number
+  ): number {
+    return recipe.ratings.filter((rating) => rating >= minRate).length;
+  }
+}
--- apps/shared/src/models/car.model.ts
+++ apps/shared/src/models/car.model.ts
@@ -1,5 +1,5 @@
 export class Car {
   constructor(public id: number, public name: string, public year: number) {
-    console.log("Car created");
+    console.log("new Car created");
   }
 }
