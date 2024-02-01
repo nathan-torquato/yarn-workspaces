39d5b4d69f1f91c3fb3cf2c2a35c0b5ea8116d06
Nathan Torquato <n.torquato.silva@gmail.com>
2024-01-31 22:29:55 -0300

    feat: add express server

--- apps/core-portal/src/index.ts
+++ apps/core-portal/src/index.ts
@@ -1,9 +1,28 @@
+import { Car } from "@src/car";
 import { Car as SharedCar } from "@shared/models";
 
-import { Car } from "@src/car";
+import express from "express";
 
 console.log("Hello from core-portal");
-const car = new Car("Ford", "Fiesta");
-console.log("local car", car);
-const sharedCar = new SharedCar(1, "Ford", 2);
-console.log("shared car", sharedCar);
+
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send("Hello from core-portal");
+});
+
+app.get("/local-car", (req, res) => {
+  const car = new Car("Ford", "Fiesta");
+  res.send(car);
+});
+
+app.get("/shared-car", (req, res) => {
+  const car = new SharedCar(2, "Ford", 3);
+  res.send(car);
+});
+
+const PORT = process.env.PORT || 38763;
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
--- apps/shared/src/models/car.model.ts
+++ apps/shared/src/models/car.model.ts
@@ -1,3 +1,5 @@
 export class Car {
-  constructor(public id: number, public name: string, public year: number) {}
+  constructor(public id: number, public name: string, public year: number) {
+    console.log("Car created");
+  }
 }
